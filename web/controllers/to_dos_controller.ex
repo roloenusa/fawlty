b1ba8ac28077ee713d11cc5b2696aa31a22b06c0
Juan Delgado <jdelgado@playmesh.com>
2015-01-14 22:27:28 -0800

    now lists can only be accessed when logged in.

--- web/controllers/to_dos_controller.ex
+++ web/controllers/to_dos_controller.ex
@@ -1,6 +1,7 @@
 defmodule Fawlty.ToDosController do
   use Phoenix.Controller
 
+  plug :authenticate, :user when action in [:index]
   plug :action
 
   def index(conn, _params) do
@@ -8,5 +9,9 @@ defmodule Fawlty.ToDosController do
     done_items = Item.find_all_completed
 
     render conn, :index, to_do_items: to_do_items, done_items: done_items
+  end
+
+  defp authenticate(conn, :user) do
+    Fawlty.SessionsController.authenticated?(conn)
   end
 end
